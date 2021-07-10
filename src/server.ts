41bab6bc6890fa8a7b2f62483cd14e2638f4bf5b
PabloMelo <pablomelodev@gmail.com>
2021-07-10 15:12:54 -0300

    feat: connection socket io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
+import path from 'path';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('socket', socket);
+});
 
 app.get('/', (request, response) => {
   return response.json({ ok: true });
 });
 
-app.listen(3333, () => console.log('Server is running'));
+server.listen(3333, () => console.log('Server is running'));
