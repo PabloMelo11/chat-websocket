3a472cf3f52148cbe13c28f90f6b47bab4318793
PabloMelo <pablomelodev@gmail.com>
2021-07-10 20:19:14 -0300

    feat: alter config room

--- src/schemas/chatRoom.ts
+++ src/schemas/chatRoom.ts
@@ -9,7 +9,7 @@ type ChatRoom = Document & {
 
 const ChatRoomSchema = new Schema({
   idUsers: [{ type: Schema.Types.ObjectId, ref: 'users' }],
-  idChatRoom: { type: String, default: uuid() },
+  idChatRoom: { type: String, default: uuid },
 });
 
 const ChatRoom = mongoose.model<ChatRoom>('chat_room', ChatRoomSchema);
